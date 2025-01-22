277d4661b7e09289918fd686ea0dd6f34e90a9aa
skuo <skuo004@ucr.edu>
2025-01-21 19:26:48 -0800

    feat: implement nowplaying command

---
+++ src/interactions/music/nowplaying.ts
@@ -0,0 +1,31 @@
+import { CommandInteraction } from 'discord.js';
+import { useQueue } from 'discord-player';
+import Interaction from '../../models/Interaction';
+
+export default class NowPlaying extends Interaction<CommandInteraction> {
+    name = 'nowplaying';
+
+    description = "What's cooking, good looking?";
+
+    options = [];
+
+    // eslint-disable-next-line class-methods-use-this
+    execute = async (interaction: CommandInteraction) => {
+        const queue = useQueue(interaction.guild!);
+
+        if (!queue) {
+            await interaction.editReply('Nope!');
+            return;
+        }
+
+        const currentTrack = queue?.currentTrack;
+
+        if (!currentTrack) {
+            await interaction.editReply('Crickets...');
+            return;
+        }
+
+        console.debug('current track', currentTrack);
+        await interaction.editReply(`Now playing: ${currentTrack.cleanTitle}`);
+    };
+}
