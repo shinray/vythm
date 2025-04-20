573c0b5ffb4f2be25884dc6bf1f336bd7d3a647b
remter <krch.addanki@gmail.com>
2025-04-19 22:02:54 -0700

    Update to address comments

--- src/interactions/music/next.ts
+++ src/interactions/music/next.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, GuildMember, VoiceChannel } from 'discord.js';
 import { useQueue } from 'discord-player';
 import Interaction from '../../models/Interaction';
 
@@ -14,12 +14,21 @@ export default class Next extends Interaction<CommandInteraction> {
         const queue = useQueue(interaction.guild!);
         const queueLength = queue?.getSize() || 0;
 
+        const member = interaction.member as GuildMember;
+        const voiceChannel = member.voice.channel as VoiceChannel;
+
+        if (!voiceChannel) {
+            await interaction.editReply(
+                "I'm too shy, I can't join on my own! You must be in a voice channel!",
+            );
+        }
+
         if (!queue || queueLength < 1) {
             await interaction.editReply('Queue is too small to skip!');
             return;
         }
 
-        if (!queue.currentTrack) {
+        if (!queue.isPlaying()) {
             await interaction.editReply('There is no track playing');
             return;
         }
